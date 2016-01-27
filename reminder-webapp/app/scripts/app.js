796e2a9cbd1b5ef7a9b7129f737032b86d7e36b8
Naveen Chandupatla <chandupatla.naveen@gmail.com>
2016-01-27 18:14:11 -0500

    First commit

---
+++ reminder-webapp/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name reminderWebappApp
+ * @description
+ * # reminderWebappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('reminderWebappApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ reminder-webapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reminderWebappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reminderWebappApp
+ */
+angular.module('reminderWebappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
